3bf457c01a79db73b6caff6d9968565bb7e003f6
BaianorASR <reis.adson2@outlook.com>
2021-11-26 17:55:16 -0300

    para fixar

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/4.1-javascript-primeiros/para-fixar/para-fixar.js
@@ -0,0 +1,10 @@
+const myName = "Adson";
+const birthCity = "Santaluz";
+let birthYear = 1998;
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+console.log(birthYear)
+
+birthCity = "São Paulo"
+console.log(birthCity)
