c7207ccca27123ccf3157d1128339ce06db5bc5d
Adison Santos Reis <baianor@pop-os.localdomain>
2021-11-28 19:39:25 -0300

    para fixar 4.2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/4.2-javascript-arrays-e-loops-for/para-fixar/exempllo.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/4.2-javascript-arrays-e-loops-for/para-fixar/exempllo.js
@@ -1,6 +1,15 @@
-let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+let tasksList = ["Tomar café", "Reunião", "Brincar com o cachorro"];
 
-tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
-console.log(tasksList);
+tasksList.push("Fazer exercícios da Trybe"); // adiciona mais uma tarefa
+tasksList.unshift("Escovar os dentes");
+tasksList.pop() // remove o ultimo item
+tasksList.shift() // remove o primero item
+console.log(tasksList); 
 
 // ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+// .pop() #remove o ultimo item do array||| .shift() remove o ultimo item do array
+
+let indexOfTask = tasksList.indexOf('Tomar café');
+console.log(indexOfTask);
+
+// 1
