467f508ae69b7859d0e21da3fe992945be6ee945
BaianorASR <reis.adson2@outlook.com>
2021-11-30 18:29:46 -0300

    part 2 finalizado, e 3 iniciada

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/4.3-javascript-logica-de-progamacao-algoritimos/exercicio/part2.js
@@ -0,0 +1,10 @@
+const n = 4;
+
+for (let index = "*"; index.length <= n; index += "*") {
+
+  // for ( let indexColuna = 1; indexColuna <= n; indexColuna++){
+  //   console.log("*")    
+  // }
+  
+  console.log(index);
+}
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/4.3-javascript-logica-de-progamacao-algoritimos/exercicio/part3.js
@@ -0,0 +1,8 @@
+const n = 5;
+let triangulo = ""
+
+for (index = 1; index <= n; index++) {
+  for (let linha = 1; linha <= n; linha++){
+    
+  };
+}
