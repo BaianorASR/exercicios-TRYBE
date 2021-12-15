536929249a468306419a036a97309e9a355d0074
BaianorASR <reis.adson2@outlook.com>
2021-12-14 21:31:59 -0300

    backup

--- fundamentos/bloco-05-javascript-dom-eventos-web-storage/5.3-javascript-eventos/exercicio/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/5.3-javascript-eventos/exercicio/script.js
@@ -135,3 +135,12 @@ for (let i of daysZoom){
 // Exercício 7:
 // Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
 // O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+const btnInput = document.querySelector('#btn-add')
+btnInput.addEventListener('click', function(event) {
+  let input = document.querySelector('#task-input')
+  criaFilho('.my-tasks', 'span', '', input.value)
+})
+// Exercício 8:
+// Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+// O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
