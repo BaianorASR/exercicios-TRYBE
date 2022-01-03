5e4113cbf8915b23afd91d88b35e39f1e14dbdd3
BaianorASR <reis.adson2@outlook.com>
2022-01-03 18:10:14 -0300

    validaçao da data ok

--- fundamentos/bloco-06-html-css-flex-flexbox-responsivo/6.1-html-css-forms/exercicio/script.js
+++ fundamentos/bloco-06-html-css-flex-flexbox-responsivo/6.1-html-css-forms/exercicio/script.js
@@ -1,3 +1,19 @@
+const inputs = {};
+
+function salvaInputs() {
+  const todosInputs = document.querySelectorAll('input');
+  for (key of todosInputs) {
+    const newObj = {};
+    newObj.required = key.required;
+    newObj.maxLength = key.maxLength;
+    newObj.value = key.value;
+    newObj.type = key.type;
+
+    const nome = key.name;
+    inputs[nome] = newObj;
+  }  
+}
+
 function states() {
   const select = document.getElementById('estado');
   const estados = [
@@ -34,16 +50,34 @@ function states() {
     options.innerText = key;
     options.value = key;
     if (key === 'BA') {
-      options.setAttribute('selected','');
+      options.setAttribute('selected', '');
     }
     select.appendChild(options);
   }
 }
 
+// https://stackoverflow.com/questions/15491894/regex-to-validate-date-formats-dd-mm-yyyy-dd-mm-yyyy-dd-mm-yyyy-dd-mmm-yyyy
+// esse regex ja faz todas as validaçoes de datas em diversos formatos
+function validaData() {
+  const regex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/gm
+    
+    if(!regex.test(inputs.data.value)){
+      window.alert('Data invalida')
+    } else {
+      console.log('data correta camarada');
+    }
+}
+
+function validaDados(event) {
+  event.preventDefault()
+  salvaInputs()
+  validaData()
+
+}
+
 window.onload = () => {
-  states()
+  states();
+  const btn_enviar = document.querySelector('#submit');
+  btn_enviar.addEventListener('click', validaDados);
 };
 
-
-const conteiner = document.createElement('section');
-document.querySelector('body').appendChild(conteiner);
