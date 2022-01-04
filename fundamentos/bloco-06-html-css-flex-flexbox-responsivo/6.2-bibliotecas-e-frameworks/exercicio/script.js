b7608785f2ef1e74bbe803a6ce8ced3ef6cbfa86
BaianorASR <reis.adson2@outlook.com>
2022-01-04 10:04:03 -0300

    innit 6.2

---
+++ fundamentos/bloco-06-html-css-flex-flexbox-responsivo/6.2-bibliotecas-e-frameworks/exercicio/script.js
@@ -0,0 +1,148 @@
+const inputs = {};
+
+function salvaInputs() {
+  const todosInputs = document.querySelectorAll('input[type=text]');
+  for (key of todosInputs) {
+    const newObj = {};
+    newObj.required = key.required;
+    newObj.maxLength = key.maxLength;
+    newObj.value = key.value;
+    newObj.type = key.type;
+    newObj.name = key.name;
+
+    const nome = key.name;
+    inputs[nome] = newObj;
+  }
+}
+
+function states() {
+  const select = document.getElementById('estado');
+  const estados = [
+    'AC',
+    'AL',
+    'AP',
+    'AM',
+    'BA',
+    'CE',
+    'DF',
+    'ES',
+    'GO',
+    'MA',
+    'MT',
+    'MS',
+    'MG',
+    'PA',
+    'PB',
+    'PR',
+    'PE',
+    'PI',
+    'RJ',
+    'RN',
+    'RS',
+    'RO',
+    'RR',
+    'SC',
+    'SP',
+    'SE',
+    'TO',
+  ];
+  for (key of estados) {
+    const options = document.createElement('option');
+    options.innerText = key;
+    options.value = key;
+    if (key === 'BA') {
+      options.setAttribute('selected', '');
+    }
+    select.appendChild(options);
+  }
+}
+
+// https://stackoverflow.com/questions/15491894/regex-to-validate-date-formats-dd-mm-yyyy-dd-mm-yyyy-dd-mm-yyyy-dd-mmm-yyyy
+// esse regex ja faz todas as validaçoes de datas em diversos formatos
+function validaData() {
+  const regex =
+    /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/gm;
+
+  if (!regex.test(inputs.data.value)) {
+    console.log('Data invalida');
+    return false;
+  }
+  return true;
+}
+
+function checkLength(key) {
+  if (key.value.length === 0) {
+    console.log(`Por Favor preencha o campo ${key.name}`);
+    return false;
+  } else if (key.value.length > key.maxLength) {
+    console.log(`o tamanho de ${key.name} esta muito grande`);
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function checkRequired() {
+  let lengthOK = false;
+  for (key in inputs) {
+    lengthOK = checkLength(inputs[key]);
+  }
+  return lengthOK;
+}
+
+function checkRadio() {
+  const radios = document.querySelectorAll('input[type=radio]');
+  let isChecked = false;
+  for (key of radios) {
+    if (key.checked) {
+      console.log('RADIO MARCADA');
+      isChecked = true;
+      break;
+    }
+  }
+  return isChecked;
+}
+
+function checkTextArea() {
+  const area = document.querySelector('#textarea');
+  if (area.value.length > area.maxLength || area.value.length === 0) {
+    console.log('TEXTEAREA');
+    return false;
+  }
+  return true;
+}
+
+function checkEstado() {
+  const optEstado = document.querySelectorAll('option');
+  let isSelected = false;
+  for (key of optEstado) {
+    if (key.selected) {
+      console.log('ESTADO MARCADO' + ' ' + key.value);
+      isSelected = true;
+      break;
+    }
+  }
+  return isSelected;
+}
+
+function validaDados(event) {
+  event.preventDefault();
+  salvaInputs();
+
+  const requiredLentgh = checkRequired();
+  const date = validaData();
+  const radio = checkRadio();
+  const textarea = checkTextArea();
+  const uhf = checkEstado();
+  if (validaData === true && checkRequired === true) {
+    console.log('Baianor joestar');
+  }
+}
+
+window.onload = () => {
+  states();
+  const btn_enviar = document.querySelector('#submit');
+  btn_enviar.addEventListener('click', validaDados);
+
+  const limpar = document.querySelector('#limpar');
+};
