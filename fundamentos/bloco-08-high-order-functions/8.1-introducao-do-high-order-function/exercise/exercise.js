90271985a83e6de75b9e762a5149f9e14111681a
BaianorASR <reis.adson2@outlook.com>
2022-01-18 18:29:49 -0300

    exercise 2

--- fundamentos/bloco-08-high-order-functions/8.1-introducao-do-high-order-function/exercise/exercise.js
+++ fundamentos/bloco-08-high-order-functions/8.1-introducao-do-high-order-function/exercise/exercise.js
@@ -9,4 +9,30 @@ const newEmployees = () => {
 
 const generateUser = (name) => ({ name, email: `${name.toLowerCase().split(' ').join('_')}@trybe.com` })
 
-console.log(newEmployees())
+// console.log(newEmployees())
+
+
+const checkSorteio = (aposta, sortNumbers) => {
+  const sortAp = [... aposta].sort();
+  const sortNb = [... sortNumbers].sort();
+  let count = 0;
+  for (let index = 1; index <= 6; index++) {
+    sortAp[index] === sortNb? count += 1 : count += 0; 
+  }
+  console.log(sortNb);
+  console.log(sortAp);
+  console.log(count);
+  return count 
+}
+
+const resultadoSorteio = (aposta, callback) => {
+  const sortNumbers = []
+  for (let index = 1; index <= 6; index += 1){
+    sortNumbers.push(Math.floor(Math.random() * 60 + 1));
+  }
+  
+  return callback(aposta, sortNumbers) === 6? "Você Ganhou" : "Que pena! tente novamente"
+  
+}
+
+console.log(resultadoSorteio([1,2,3,4,5,6], checkSorteio))
