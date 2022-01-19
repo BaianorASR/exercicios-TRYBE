040c887656029febe0d5b156269120856c6e1208
BaianorASR <reis.adson2@outlook.com>
2022-01-19 10:54:11 -0300

    reafatorado exercise 2

--- fundamentos/bloco-08-high-order-functions/8.1-introducao-do-high-order-function/exercise/exercise.js
+++ fundamentos/bloco-08-high-order-functions/8.1-introducao-do-high-order-function/exercise/exercise.js
@@ -11,29 +11,13 @@ const generateUser = (name) => ({ name, email: `${name.toLowerCase().split(' ').
 
 // console.log(newEmployees())
 
-const checkSorteio = (aposta, sortNumbers) => {
-  const sortAp = [...aposta].sort();
-  const sortNb = [...sortNumbers].sort();
-  let count = 0;
-  for (let index = 1; index <= 6; index++) {
-    sortAp[index] === sortNb ? (count += 1) : (count += 0);
-  }
-  console.log(sortNb);
-  console.log(sortAp);
-  console.log(count);
-  return count;
-};
+const checkSorteio = (number, myNumber) => number === myNumber;
+const sorteio = (myNumber, callback) => {
+  const number = Math.floor((Math.random() * 5) + 1)
+  return callback(number, myNumber) ? 'Parabéns, você ganhou' : 'Que pena :( continue tentando'
+}
 
-const resultadoSorteio = (aposta, callback) => {
-  const sortNumbers = [];
-  for (let index = 1; index <= 6; index += 1) {
-    sortNumbers.push(Math.floor(Math.random() * 60 + 1));
-  }
-
-  return callback(aposta, sortNumbers) === 6 ? 'Você Ganhou' : 'Que pena! tente novamente';
-};
-
-// console.log(resultadoSorteio([1,2,3,4,5,6], checkSorteio))
+console.log(sorteio(2, checkSorteio))
 
 const checkNota = (gabarito, studMark) => {
   let count = 0;
