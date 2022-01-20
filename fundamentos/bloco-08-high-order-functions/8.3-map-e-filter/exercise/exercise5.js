9af34a4157e7a530b7af477bf371504ae25df430
BaianorASR <reis.adson2@outlook.com>
2022-01-20 19:56:28 -0300

    exercicios finalizados

--- fundamentos/bloco-08-high-order-functions/8.3-map-e-filter/exercise/exercise5.js
+++ fundamentos/bloco-08-high-order-functions/8.3-map-e-filter/exercise/exercise5.js
@@ -2,6 +2,6 @@ import { books } from './books.js';
 
 // * 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
 
-function fantasyOrScienceFictionAuthors() {
-  // escreva seu código aqui
-}
+const fantasyOrScienceFictionAuthors = () => books.filter((e) => e.genre === 'Fantasia' || e.genre === 'Ficção Científica').map((e) => e.author.name).sort()
+
+console.log(fantasyOrScienceFictionAuthors());
--- fundamentos/bloco-08-high-order-functions/8.3-map-e-filter/exercise/exercise6.js
+++ fundamentos/bloco-08-high-order-functions/8.3-map-e-filter/exercise/exercise6.js
@@ -2,6 +2,6 @@ import { books } from './books.js';
 
 // * 6 - Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
 
-function oldBooks() {
-  // escreva seu código aqui
-}
+const oldBooks = () => books.filter((e) => 2022 - e.releaseYear >= 60).map((e) => e.name);
+
+console.log(oldBooks())
