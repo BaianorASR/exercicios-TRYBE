08d90d46e6bf95b2cb31f18bf435409f25cbb24c
BaianorASR <reis.adson2@outlook.com>
2022-01-20 20:01:21 -0300

    refactor code exercise 7

--- fundamentos/bloco-08-high-order-functions/8.3-map-e-filter/exercise/exercise7.js
+++ fundamentos/bloco-08-high-order-functions/8.3-map-e-filter/exercise/exercise7.js
@@ -3,7 +3,7 @@ import { books } from './books.js';
 // * 7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
 // Dica: cada inicial termina com um ponto.
 
-const regex = /^\w\.\s\w\.\s\w\./g
+const regex = /^\w\.\s\w\.\s\w\.\s/g
 const authorWith3DotsOnName = () => books.find((e) => regex.test(e.author.name)).name;
 
 console.log(authorWith3DotsOnName());
