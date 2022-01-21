a6336e9855c7e320fe6b8af1aaba1d188a3d83ef
BaianorASR <reis.adson2@outlook.com>
2022-01-20 22:22:52 -0300

    resolve git bug

--- fundamentos/bloco-08-high-order-functions/8.3-map-e-filter/exercise/exercise1.js
+++ fundamentos/bloco-08-high-order-functions/8.3-map-e-filter/exercise/exercise1.js
@@ -1,6 +1,6 @@
 import { books } from './books.js';
 
-// Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+// * Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
 
 const formatedBookNames = () => books.map(item => `${item.name} - ${item.genre} - ${item.author.name}`)
 
