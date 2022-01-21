b2aafae8de16c58cf71a812bff9f40b67fdf2b90
BaianorASR <reis.adson2@outlook.com>
2022-01-20 22:19:43 -0300

    end day

--- fundamentos/bloco-08-high-order-functions/8.3-map-e-filter/exercise/exercise3.js
+++ fundamentos/bloco-08-high-order-functions/8.3-map-e-filter/exercise/exercise3.js
@@ -3,6 +3,6 @@ import { books } from './books.js';
 // * 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
 // Dica: use a função filter ;
 
-const fantasyOrScienceFiction = () => books.filter((element) => element.genre === "Fantasia" || element.genre === 'Ficção Científica');
+const fantasyOrScienceFiction = () => books.filter((e) => e.genre === "Fantasia" || e.genre === 'Ficção Científica');
 
 console.log(fantasyOrScienceFiction());
--- fundamentos/bloco-08-high-order-functions/8.3-map-e-filter/exercise/exercise4.js
+++ fundamentos/bloco-08-high-order-functions/8.3-map-e-filter/exercise/exercise4.js
@@ -3,6 +3,6 @@ import { books } from './books.js';
 // * 4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
 // Dica: use as funções filter e sort
 
-const oldBooksOrdered = () => books.filter((element) => 2022 - element.releaseYear >= 60).sort((a, b) => a.releaseYear - b.releaseYear)
+const oldBooksOrdered = () => books.filter((e) => 2022 - e.releaseYear >= 60).sort((a, b) => a.releaseYear - b.releaseYear)
 
 console.log(oldBooksOrdered())
