f2a301a156db82ab766cd61d99ae4f9f05227f6d
BaianorASR <reis.adson2@outlook.com>
2022-02-07 14:13:22 -0300

    setup react

---
+++ front-end/bloco-10-introducao-react/10.1-hello-world-react/parafixar/parafixar/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Ads from './component';
+
+
+function App() {
+  return <Ads />;
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-react/10.1-hello-world-react/parafixar/parafixar/src/component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Ads extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Adson Reis</h1>
+        <h2>Baianor Joestar</h2>
+        <p>Esdudante FullStack na Trybe</p>
+      </div>
+    );
+  }
+}
+
+export default Ads;
